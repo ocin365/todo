562e75fc2472de50716c4c804e0c0d480e7daa76
ocin365 <nicokriel2021@gmail.com>
2025-03-25 06:16:34 +0200

    lightmode bug fixed

--- script.js
+++ script.js
@@ -87,12 +87,14 @@ let lightmode = localStorage.getItem('lightmode');
 const themeSwitch = document.getElementById('theme-switch');
 
 const enableLightMode = () => {
+
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
 
 }
 
 const disableLightMode = () => {
+
     document.body.classList.remove('lightmode');
     localStorage.removeItem('lightmode', null);
 }
